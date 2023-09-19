fa707291f342121e49de0d636b9a511f82ed1c60
Giácomo Antônio Althoff Bolan <kinhobolan@live.com>
2023-09-19 15:27:18 -0300

    add ready func

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,6 +11,9 @@ const JUMP_VELOCITY = -320.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	print("ready")
+
 
 func _physics_process(delta):
 	# Add the gravity.
